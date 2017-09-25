b9da9fac28b876409a2bcf1845e5c42b9920ac64
vishnu <vishnu.cherumadathil@cesltd.com>
2017-09-25 19:39:19 +0530

    updated for zip file

--- imageupload/views.py
+++ imageupload/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .serializers import UploadSerializer
 from .models import *
 from django.shortcuts import get_object_or_404
+import zipfile
 
 # Create your views here.
 
@@ -16,13 +17,18 @@ class DataUploadViewSet(viewsets.ViewSet):
         return Response({'status': 'ok'})
 
     def create(self, request):
-        serializer_class = UploadSerializer(data=request.data)
-        if serializer_class.is_valid():
-            serializer_class.save()
+        if request.data['file_type'] == 'zip':
+            zip_ref = zipfile.ZipFile(request.data['image'], 'r')
+            print zip_ref
             return Response(status.HTTP_201_CREATED)
         else:
-            return Response({'status': status.HTTP_400_BAD_REQUEST,
-                             'error': serializer_class.errors})
+            serializer_class = UploadSerializer(data=request.data)
+            if serializer_class.is_valid():
+                serializer_class.save()
+                return Response(status.HTTP_201_CREATED)
+            else:
+                return Response({'status': status.HTTP_400_BAD_REQUEST,
+                                 'error': serializer_class.errors})
 
     def update(self, request, pk=None):
         image_upload = get_object_or_404(ImageUpload, id=pk)
